223abb399e1da2bb745adf33c026366895d1e9a1
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2024-12-29 22:48:06 +0600

    me notes saved

--- app/src/main/java/com/oakil/notessqlite/AddNoteActivity.kt
+++ app/src/main/java/com/oakil/notessqlite/AddNoteActivity.kt
@@ -1,14 +1,36 @@
 package com.oakil.notessqlite
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.oakil.notessqlite.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
+   private lateinit var binding: ActivityAddNoteBinding
+   private lateinit var database: NotesDatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_add_note)
+        setContentView(binding.root)
+
+        database = NotesDatabaseHelper(this)
+
+
+
+        binding.saveButton.setOnClickListener{
+            val title = binding.titleEditText.text.toString()
+            val content = binding.contentEditText.text.toString()
+            val note = Note(0, title, content)
+
+            database.insertNote(note)
+            finish()
+            Toast.makeText(this, "notes saved" , Toast.LENGTH_SHORT).show()
+
+
+        }
 
     }
 }
